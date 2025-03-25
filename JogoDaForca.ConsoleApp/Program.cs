736ca8a684ad8e75641fa7ced3665605477cbc78
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-03-25 20:16:38 -0300

    Add acerto da palavra no jogo

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -4,31 +4,50 @@
     {
         static void Main(string[] args)
         {
-            string palavraSecreta = "MELANCIA";
-
-            char[] letrasEncontradas = new char[palavraSecreta.Length];
-
-            for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
+            while (true)
             {
-                // acessar o array no índice 0
-                letrasEncontradas[caractere] = '_';
+                string palavraSecreta = "MELANCIA";
+
+                char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+                for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
+                {
+                    // acessar o array no índice 0
+                    letrasEncontradas[caractere] = '_';
+                }
+
+                int quantidadeErros = 0;
+                bool jogadorEnforcou = false;
+                bool jogadorAcertou = false;
+
+                do
+                {
+                    string dicaDaPalavra = String.Join(" ", letrasEncontradas);
+
+                    Console.Clear();
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Jogo da Forca");
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine($"Palavra secreta: {dicaDaPalavra}");
+                    Console.WriteLine("---------------------------------------");
+
+                    Console.Write("Digite uma letra: ");
+                    char chute = Console.ReadLine()[0]; // obtém apenas um caractere que o usuário digitou
+
+                    for (int contador = 0; contador < palavraSecreta.Length; contador++)
+                    {
+                        char letraAtual = palavraSecreta[contador];
+
+                        if (chute == letraAtual)
+                        {
+                            letrasEncontradas[contador] = letraAtual;
+                        }
+                    }
+
+                    Console.ReadLine();
+
+                } while (jogadorAcertou == false || jogadorEnforcou == false);
             }
-
-            string dicaDaPalavra = String.Join(" ", letrasEncontradas);
-
-            Console.Clear();
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine("Jogo da Forca");
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine($"Palavra secreta: {dicaDaPalavra}");
-            Console.WriteLine("---------------------------------------");
-
-            Console.Write("Digite uma letra: ");
-            char chute = Console.ReadLine()[0]; // obtém apenas um caractere que o usuário digitou
-
-            Console.WriteLine(chute);
-
-            Console.ReadLine();
         }
     }
 }
