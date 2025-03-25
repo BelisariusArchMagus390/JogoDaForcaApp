09fdd687d57f471ce95fa5751915181dffe46582
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-03-25 19:50:19 -0300

    Criado a exibição de dica da palavra secreta

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string palavraSecreta = "MELANCIA";
+
+            char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+            for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
+            {
+                // acessar o array no índice 0
+                letrasEncontradas[caractere] = '_';
+            }
+
+            string dicaDaPalavra = String.Join(" ", letrasEncontradas);
+
+            Console.Clear();
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Jogo da Forca");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"Palavra secreta: {dicaDaPalavra}");
+            Console.WriteLine("---------------------------------------");
+
+            Console.Write("Digite uma letra: ");
+            char chute = Console.ReadLine()[0]; // obtém apenas um caractere que o usuário digitou
+
+            Console.WriteLine(chute);
+
+            Console.ReadLine();
         }
     }
 }
