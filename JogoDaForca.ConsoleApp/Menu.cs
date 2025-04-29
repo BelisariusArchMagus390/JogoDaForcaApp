5cf01ae5212c2fa6d44e5635c1d7813b4e22f65a
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-04-29 13:45:47 -0300

    Movido os métodos mostrarMenuJogo(), opcaoDecisao(0, opcaoSaida(), mensagemErroOpcao() para a classe Menu

--- JogoDaForca.ConsoleApp/Menu.cs
+++ JogoDaForca.ConsoleApp/Menu.cs
@@ -8,5 +8,76 @@ namespace JogoDaForca.ConsoleApp
 {
     internal class Menu
     {
+        static void mostrarMenuJogo(string dicaDaPalavra, int qtErros, char[] letrasDigitadas)
+        {
+            Console.WriteLine(" ---------------------------------------");
+            Console.WriteLine(" Máximo de erros: 5");
+            Console.WriteLine(" ---------------------------------------");
+            Console.WriteLine($" Palavra secreta: {dicaDaPalavra}");
+            Console.WriteLine(" ---------------------------------------");
+
+            int tamanho = letrasDigitadas.Length;
+
+            Console.Write(" Letras já usadas: ");
+
+            for (int letra = 0; letra < tamanho; letra++)
+            {
+                if (letrasDigitadas[letra] != '_')
+                {
+                    if ((letra + 1) < tamanho)
+                        if (letrasDigitadas[letra + 1] != '_')
+                            Console.Write($"{letrasDigitadas[letra]} | ");
+                        else
+                            Console.Write($"{letrasDigitadas[letra]}");
+                }
+            }
+
+            Console.WriteLine("\n ---------------------------------------");
+            Console.WriteLine($" Quantidade de erros: {qtErros}");
+        }
+
+        static char opcaoDecisao()
+        {
+            char opcaoResposta;
+
+            Console.WriteLine(" ---------------------------------------");
+            Console.WriteLine(" 1 - Palavra");
+            Console.WriteLine(" 2 - Letra");
+            Console.WriteLine(" ---------------------------------------");
+            Console.Write(" Escolha se quer responder com uma palavra ou letra: ");
+            opcaoResposta = Console.ReadLine()[0];
+
+            return opcaoResposta;
+        }
+
+        static char opcaoSaida()
+        {
+            char opcao;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.Write(" Gostaria de jogar de novo?(S/N): ");
+                opcao = Console.ReadLine()[0];
+                opcao = Char.ToUpper(opcao);
+
+                if (opcao != 'S' && opcao != 'N')
+                {
+                    mensagemErroOpcao();
+                }
+                else
+                    break;
+            }
+
+            return opcao;
+        }
+
+        static void mensagemErroOpcao()
+        {
+            Console.Clear();
+            Console.WriteLine(" Erro! Essa escolha não existe, favor escolher uma que exista.");
+            Console.WriteLine(" Aperte Enter para continuar...");
+            Console.ReadLine();
+        }
     }
 }
--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -121,36 +121,6 @@
             }
         }
 
-        static void mensagemErroOpcao()
-        {
-            Console.Clear();
-            Console.WriteLine(" Erro! Essa escolha não existe, favor escolher uma que exista.");
-            Console.WriteLine(" Aperte Enter para continuar...");
-            Console.ReadLine();
-        }
-
-        static char opcaoSaida()
-        {
-            char opcao;
-
-            while (true)
-            {
-                Console.Clear();
-                Console.Write(" Gostaria de jogar de novo?(S/N): ");
-                opcao = Console.ReadLine()[0];
-                opcao = Char.ToUpper(opcao);
-
-                if (opcao != 'S' && opcao != 'N')
-                {
-                    mensagemErroOpcao();
-                }
-                else
-                    break;
-            }
-
-            return opcao;
-        }
-
         static char[] constroiArrayVazio(int tamanhoArray, char[] array)
         {
             for (int caractere = 0; caractere < tamanhoArray; caractere++)
@@ -266,34 +236,6 @@
             return letrasPalavraSecreta.Length - qtEspacosVazios(letrasPalavraSecreta) + qtErrosMaximo;
         }
 
-        static void mostrarMenuJogo(string dicaDaPalavra, int qtErros, char [] letrasDigitadas)
-        {
-            Console.WriteLine(" ---------------------------------------");
-            Console.WriteLine(" Máximo de erros: 5");
-            Console.WriteLine(" ---------------------------------------");
-            Console.WriteLine($" Palavra secreta: {dicaDaPalavra}");
-            Console.WriteLine(" ---------------------------------------");
-
-            int tamanho = letrasDigitadas.Length;
-
-            Console.Write(" Letras já usadas: ");
-
-            for (int letra = 0; letra < tamanho; letra++)
-            {
-                if (letrasDigitadas[letra] != '_')
-                {
-                    if ((letra+1) < tamanho)
-                        if (letrasDigitadas[letra+1] != '_')
-                            Console.Write($"{letrasDigitadas[letra]} | ");
-                        else
-                            Console.Write($"{letrasDigitadas[letra]}");
-                }
-            }
-
-            Console.WriteLine("\n ---------------------------------------");
-            Console.WriteLine($" Quantidade de erros: {qtErros}");
-        }
-
         // verifica se a letra pode ser adicionada no array
         static bool letraPodeEntrar(char chute, char[] letrasDigitadas)
         {
@@ -310,20 +252,6 @@
                 return true;
             else
                 return false;
-        }
-
-        static char opcaoDecisao()
-        {
-            char opcaoResposta;
-
-            Console.WriteLine(" ---------------------------------------");
-            Console.WriteLine(" 1 - Palavra");
-            Console.WriteLine(" 2 - Letra");
-            Console.WriteLine(" ---------------------------------------");
-            Console.Write(" Escolha se quer responder com uma palavra ou letra: ");
-            opcaoResposta = Console.ReadLine()[0];
-
-            return opcaoResposta;
         }
 
         static string escolhaPalavraSecreta()
